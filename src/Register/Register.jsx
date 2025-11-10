019b3019f6c532d794d14c93bddbba84014ae920
tufayel2007 <ta07092007@gmail.com>
2025-11-10 15:55:22 +0600

    update samthingh function and samthingh design add

--- src/Register/Register.jsx
+++ src/Register/Register.jsx
@@ -389,7 +389,7 @@ const Register = () => {
         </div>
 
         <p className="text-center mt-8 text-gray-600">
-          Have an account?
+          Have an accou
           <Link
             to="/login"
             className="font-bold text-emerald-600 hover:text-emerald-800 hover:underline transition"
