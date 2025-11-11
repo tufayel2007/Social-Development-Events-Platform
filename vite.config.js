7970dfb203c49891cdf42370dacc7ffa3a1099e8
tufayel2007 <ta07092007@gmail.com>
2025-11-11 14:04:36 +0600

    add vercel rewrite rule

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
-// vite.config.js
+// vite.config.js (একদম ফাইনাল ভার্সন)
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react()], // শুধু react() দরকার
+  plugins: [react(), tailwindcss()],
 
+  // এই অংশটা যোগ করো (সবচেয়ে গুরুত্বপূর্ণ!)
   server: {
     proxy: {
       "/api": {
